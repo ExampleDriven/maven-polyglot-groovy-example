71426f7e3164ac349c0c81f28ef7147f7037d937
Peter Szanto <git@szantocsalad.hu>
2017-01-11 09:51:04 +0100

    workaround for bug https://github.com/takari/polyglot-maven/issues/37

---
+++ maven-polyglot-submodule/pom.groovy
@@ -0,0 +1,111 @@
+project {
+
+    modelVersion '4.0.0'
+// Temporarily commented out until this https://github.com/takari/polyglot-maven/issues/37 gets resolved
+//    parent {
+//        groupId 'org.exampledriven'
+//        artifactId 'maven-polyglot-groovy-example-parent'
+//        version '1.0-SNAPSHOT'
+//    }
+
+    groupId 'org.exampledriven'
+    version '1.0-SNAPSHOT'
+    artifactId 'maven-polyglot-submodule'
+
+    properties {
+        'greet' 'true'
+        'greet2' 'true'
+    }
+
+    build {
+
+        $execute(id: 'hello', phase: 'compile') {
+
+            if ("true".equals(System.getProperty("file-test"))) {
+                println "File generation is enabled"
+
+                println properties.size()
+                println this.getProperties().containsKey('greet')
+                println properties
+                println properties.get('greet')
+
+                if (getProperties().getOrDefault('greet', false)) {
+                    System.out.println 'Hello from groovy'
+                }
+
+                def directory = "target/classes/new"
+                def dirCreated = new File(directory).mkdir();
+
+                println "Directory was created? " + dirCreated
+                def file = new File(directory + "/hello.txt")
+
+                if (!file.exists()) {
+                    println "Creating hello.txt"
+
+                    file.createNewFile();
+                    file.write("hello from groovy")
+
+                } else {
+                    println "hello.txt is already created"
+
+                }
+            } else {
+                println "File generation is disabled"
+            }
+
+        }
+
+    }
+
+    profiles {
+        profile {
+            activation {
+//                activeByDefault 'true'
+                property {
+                    name 'greet'
+                }
+            }
+            build {
+                plugins {
+                    plugin {
+                        groupId 'com.github.exampledriven'
+                        artifactId 'hierarchy-maven-plugin'
+                        version '1.4'
+                        executions {
+                            execution {
+                                id 'install'
+                                phase 'install'
+
+                                goals 'tree'
+
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        profile {
+            id 'javadoc'
+            activation {
+                property {
+                    name 'javadoc'
+                }
+            }
+            build {
+                plugins {
+                    plugin {
+                        artifactId 'maven-javadoc-plugin'
+                        version '2.10.4'
+                        executions {
+                            execution {
+                                id 'test'
+                                phase 'install'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- pom.groovy
+++ pom.groovy
@@ -1,11 +1,12 @@
 project {
   modelVersion '4.0.0'
   groupId 'org.exampledriven'
-  artifactId 'maven-polyglot-groovy-example'
+  artifactId 'maven-polyglot-groovy-example-parent'
   version '1.0-SNAPSHOT'
   packaging 'pom'
   modules {
-    module 'module1'
+    module 'maven-polyglot-submodule'
     module 'maven-polyglot-spring'
   }
+
 }
